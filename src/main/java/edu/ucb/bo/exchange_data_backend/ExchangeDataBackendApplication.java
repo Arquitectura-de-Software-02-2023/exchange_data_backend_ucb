200cf2a4b1699918fa845af0564cd9eb3ba4de44
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-08-15 21:18:57 -0400

    first commit

---
+++ src/main/java/edu/ucb/bo/exchange_data_backend/ExchangeDataBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.ucb.bo.exchange_data_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeDataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeDataBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ucb/bo/exchange_data_backend/ExchangeDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucb.bo.exchange_data_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeDataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
