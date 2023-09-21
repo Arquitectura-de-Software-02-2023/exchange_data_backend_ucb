64a7b0076bb02260adb4a5e26a33ac0b2232d19f
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-09-21 18:09:47 -0400

    Security config implemented with Keycloack

---
+++ src/main/java/edu/ucb/bo/exchange_data_backend/Config/GlobalSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package edu.ucb.bo.exchange_data_backend.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+public class GlobalSecurityConfiguration {
+
+    private final KeycloakJwtTokenConverter keycloakJwtTokenConverter;
+
+    public GlobalSecurityConfiguration(TokenConverterProperties properties) {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter
+                = new JwtGrantedAuthoritiesConverter();
+        this.keycloakJwtTokenConverter
+                = new KeycloakJwtTokenConverter(
+                jwtGrantedAuthoritiesConverter,
+                properties);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests( (authorizeHttpRequests) -> {
+            authorizeHttpRequests
+//                    .requestMatchers("/api/v1/currency/free").permitAll()
+//                    .requestMatchers("/api/v1/currency/hello").hasRole("USER")
+                    .anyRequest()
+                    .permitAll();
+//                    .authenticated();
+        });
+        http.oauth2ResourceServer( (oauth2) -> {
+            oauth2.jwt( (jwt) -> jwt.jwtAuthenticationConverter(keycloakJwtTokenConverter));
+        });
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
---
+++ src/main/java/edu/ucb/bo/exchange_data_backend/Config/TokenConverterProperties.java
@@ -0,0 +1,33 @@
+package edu.ucb.bo.exchange_data_backend.Config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Configuration
+@ConfigurationProperties(prefix = "token.converter")
+public class TokenConverterProperties {
+    private String resourceId;
+    private String principalAttribute;
+
+    public TokenConverterProperties() {
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public Optional<String> getPrincipalAttribute() {
+        return Optional.ofNullable(principalAttribute);
+    }
+
+    public void setPrincipalAttribute(String principalAttribute) {
+        this.principalAttribute = principalAttribute;
+    }
+}
